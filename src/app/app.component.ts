90eaaaa37a84828e4a512816621a3d1a3c12d588
daniel <danielafa.9811hp@gmail.com>
2019-07-23 23:40:59 -0500

    22-julio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,ViewChild,ElementRef } from '@angular/core';
+
 //SERVICIOS
 import {InformacionService} from './services/informacion.service';
+
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   constructor(public _is:InformacionService){
 
   }
+  ngOnInit(): void {
+    
+  }
+
 }
--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Expo} from 'gsap/all';
 //SERVICIOS
 import {InformacionService} from '../../services/informacion.service';
+//variables globales
+declare var TweenMax: any;
+declare var $: any;
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -11,6 +15,38 @@ export class HeaderComponent implements OnInit {
   constructor(public _is:InformacionService) { }
 
   ngOnInit() {
+    this.animacionHeader();
+    this.menuToggle();
+  }
+  animacionHeader(): void {
+    TweenMax.from("#brand", 2, {
+      delay: 0.4,
+      y: 10,
+      opacity: 0,
+      ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom("#menu li a", 4, {
+      delay: 0.4,
+      opacity: 0,
+      ease: Expo.easeInOut
+}, 0.1);
   }
 
+  menuToggle(){
+    $("#toggle").click(function() {
+      $(this).toggleClass('on');
+      $("#resize").toggleClass("active");
+  });
+  
+  $("#resize ul li a").click(function() {
+      $(this).toggleClass('on');
+      $("#resize").toggleClass("active");
+  });
+  
+  $(".close-btn").click(function() {
+      $(this).toggleClass('on');
+      $("#resize").toggleClass("active");
+  });
+  }
 }
